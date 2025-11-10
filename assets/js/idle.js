b670d8f076fda1e66e40392715a2e6ae43570c8b
ArnasSo <eaa25arso@students.eaaa.dk>
2025-11-10 09:15:07 +0100

    some link fixes

--- assets/js/idle.js
+++ assets/js/idle.js
@@ -4,7 +4,7 @@ function resetTimer() {
   clearTimeout(idleTimer);
   idleTimer = setTimeout(() => {
     window.location.href = "screensaver.html";
-  }, 10000); // 10000 ms = 10 seconds
+  }, 20000); // 20000 ms = 10 seconds
 }
 
 // Reset timer on common activity
